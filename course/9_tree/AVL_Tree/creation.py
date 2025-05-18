ca031fe1dc82c95113678869c4a635ee9da61525
abhidharsh <abhidharsh1999@gmail.com>
2025-05-18 20:38:01 +0530

    insertion in AVL Tree is completed

--- course/9_tree/AVL_Tree/creation.py
+++ course/9_tree/AVL_Tree/creation.py
@@ -68,6 +68,12 @@ class Queue:
         else:
             self.queue.tail.next = self.queue.tail = new_node
 
+    def __str__(self):
+        result = []
+        for i in self.queue:
+            result.append(i)
+        return " ".join(result)
+
     def dequeue(self):
         if self.IsEmpty():
             return "Empty Queue"
@@ -88,13 +94,13 @@ def LevelorderTraversal(RootNode):
     else:
         queue = Queue()
         queue.enqueue(RootNode)
-        while queue:
+        while not queue.IsEmpty():
             root = queue.dequeue()
             print(root.value.data)
-            if RootNode.value.leftchild is not None:
-                queue.enqueue(RootNode.value.leftchild)
-            if RootNode.value.rightchild is not None:
-                queue.enqueue(RootNode.value.rightchild)
+            if root.value.leftchild is not None:
+                queue.enqueue(root.value.leftchild)
+            if root.value.rightchild is not None:
+                queue.enqueue(root.value.rightchild)
 
 
 def searching(RootNode, SearchValue):
@@ -115,4 +121,72 @@ def searching(RootNode, SearchValue):
                 return "Value does not exist"
 
 
-new_AVL = AVL_Node(10)
+def getHeight(RootNode):
+    if not RootNode:
+        return 0
+    return RootNode.height
+
+
+def rightRotate(disbalancedNode):
+    newRoot = disbalancedNode.leftchild
+    disbalancedNode.leftchild = disbalancedNode.leftchild.rightchild
+    newRoot.rightchild = disbalancedNode
+    # here we are adding 1 because we have getting the height of left and rightchild so 1 more to be incremented to get the proper height of it
+    disbalancedNode.height = 1 + max(
+        getHeight(disbalancedNode.leftchild), getHeight(disbalancedNode.rightchild)
+    )
+    # set newRoot height
+    newRoot.height = 1 + max(
+        getHeight(newRoot.leftchild), getHeight(newRoot.rightchild)
+    )
+    return newRoot
+
+
+def leftRotate(disbalancedNode):
+    newRoot = disbalancedNode.rightchild
+    disbalancedNode.rightchild = disbalancedNode.rightchild.leftchild
+    newRoot.leftchild = disbalancedNode
+    disbalancedNode.height = 1 + max(
+        getHeight(disbalancedNode.leftchild), getHeight(disbalancedNode.rightchild)
+    )
+    newRoot.height = 1 + max(
+        getHeight(newRoot.leftchild), getHeight(newRoot.rightchild)
+    )
+    return newRoot
+
+
+def getBalance(RootNode):
+    if not RootNode:
+        return 0
+    return getHeight(RootNode.leftchild) - getHeight(RootNode.rightchild)
+
+
+def insertNode(RootNode, nodeValue):
+    if not RootNode:
+        return AVL_Node(nodeValue)
+    elif nodeValue < RootNode.data:
+        RootNode.leftchild = insertNode(RootNode.leftchild, nodeValue)
+    else:
+        RootNode.rightchild = insertNode(RootNode.rightchild, nodeValue)
+    RootNode.height = 1 + max(
+        getHeight(RootNode.leftchild), getHeight(RootNode.rightchild)
+    )
+    balance = getBalance(RootNode)
+    if balance > 1 and nodeValue < RootNode.leftchild.data:
+        return rightRotate(RootNode)
+    if balance > 1 and nodeValue > RootNode.leftchild.data:
+        RootNode.leftchild = leftRotate(RootNode.leftchild)
+        return rightRotate(RootNode)
+    if balance < -1 and nodeValue > RootNode.rightchild.data:
+        return leftRotate(RootNode)
+    if balance < -1 and nodeValue < RootNode.rightchild.data:
+        RootNode.rightchild = rightRotate(RootNode.rightchild)
+        return leftRotate(RootNode)
+    return RootNode
+
+
+new_AVL = AVL_Node(5)
+new_AVL = insertNode(new_AVL, 10)
+new_AVL = insertNode(new_AVL, 15)
+new_AVL = insertNode(new_AVL, 20)
+LevelorderTraversal(new_AVL)
