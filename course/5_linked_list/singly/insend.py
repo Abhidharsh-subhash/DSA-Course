d762b5d6b17688880be2a36c66d48cf4f747cdf7
abhidharsh <abhidharsh1999@gmail.com>
2025-06-15 21:52:55 +0530

    some minor changes while revision

--- course/5_linked_list/singly/insend.py
+++ course/5_linked_list/singly/insend.py
@@ -13,11 +13,9 @@ class LinkedList:
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
-            self.head = new_node
-            self.tail = new_node
+            self.head = self.tail = new_node
         else:
-            self.tail.next = new_node
-            self.tail = new_node
+            self.tail.next = self.tail = new_node
         self.length += 1
 
 
