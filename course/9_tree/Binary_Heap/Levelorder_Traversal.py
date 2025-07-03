f35d2bbe2a807418cf879310a660a6ecf00a0aa7
abhidharsh <abhidharsh1999@gmail.com>
2025-07-03 21:53:21 +0530

    insertion in binary heap is completed

--- course/9_tree/Binary_Heap/Levelorder_Traversal.py
+++ course/9_tree/Binary_Heap/Levelorder_Traversal.py
@@ -28,3 +28,40 @@ def LevelorderTraversal(RootNode):
     else:
         for i in range(1, RootNode.HeapSize + 1):
             print(RootNode.CustomList[i])
+
+
+def HeapifyTreeInsert(RootNode, Index, HeapType):
+    parent = int(Index / 2)
+    if Index <= 1:
+        return
+    if HeapType == "Min":
+        if RootNode.CustomList[Index] < RootNode.CustomList[parent]:
+            RootNode.CustomList[Index], RootNode.CustomList[parent] = (
+                RootNode.CustomList[parent],
+                RootNode.CustomList[Index],
+            )
+        HeapifyTreeInsert(RootNode, parent, HeapType)
+    elif HeapType == "Max":
+        if RootNode.CustomList[Index] > RootNode.CustomList[parent]:
+            RootNode.CustomList[Index], RootNode.CustomList[parent] = (
+                RootNode.CustomList[parent],
+                RootNode.CustomList[Index],
+            )
+        HeapifyTreeInsert(RootNode, parent, HeapType)
+
+
+def InsertNode(RootNode, NodeValue, HeapType):
+    if RootNode.HeapSize + 1 == RootNode.MaxSize:
+        return "The Binary Heap is full"
+    RootNode.CustomList[RootNode.HeapSize + 1] = NodeValue
+    RootNode.HeapSize += 1
+    HeapifyTreeInsert(RootNode, RootNode.HeapSize, HeapType)
+    return "Node inserted successfully"
+
+
+NewHeap = BinaryHeap(5)
+InsertNode(NewHeap, 4, "Max")
+InsertNode(NewHeap, 5, "Max")
+InsertNode(NewHeap, 2, "Max")
+InsertNode(NewHeap, 1, "Max")
+LevelorderTraversal(NewHeap)
--- course/9_tree/Binary_Heap/insertion.py
+++ course/9_tree/Binary_Heap/insertion.py
@@ -1,27 +1,65 @@
 class BinaryHeap:
     def __init__(self, size):
-        self.MaxSize = size + 1
-        self.CustomList = self.MaxSize * [None]
-        self.HeapSize = 0
+        self.maxsize = size + 1
+        self.customlist = self.maxsize * [None]
+        self.heapsize = 0
 
 
-def peek(RootNode):
-    if not RootNode:
+def peek(rootnode):
+    if not rootnode:
         return
     else:
-        return RootNode.CustomList[1]
+        return rootnode.customlist[1]
 
 
-def HeapSize(RootNode):
-    if not RootNode:
+def HeapSize(rootnode):
+    if not rootnode:
         return
     else:
-        return RootNode.HeapSize
+        return rootnode.heapsize
 
 
-def LevelOrderTraversal(RootNode):
-    if not RootNode:
+def levelordertraversal(rootnode):
+    if not rootnode:
         return
     else:
-        for i in range(1, RootNode.HeapSize + 1):
-            print(RootNode.CustomList[i])
+        for i in range(1, rootnode.heapsize + 1):
+            print(rootnode.customlist[i])
+
+
+def heapifytree(rootnode, index, heaptype):
+    parent = int(index / 2)
+    if index <= 1:
+        return
+    if heaptype == "Max":
+        if rootnode.customlist[index] > rootnode.customlist[parent]:
+            rootnode.customlist[index], rootnode.customlist[parent] = (
+                rootnode.customlist[parent],
+                rootnode.customlist[index],
+            )
+        heapifytree(rootnode, parent, heaptype)
+    elif heaptype == "Min":
+        if rootnode.customlist[index] < rootnode.customlist[parent]:
+            rootnode.customlist[index], rootnode.customlist[parent] = (
+                rootnode.customlist[parent],
+                rootnode.customlist[index],
+            )
+        heapifytree(rootnode, parent, heaptype)
+
+
+def Insert(rootnode, nodevalue, heaptype):
+    if rootnode.heapsize + 1 == rootnode.maxsize:
+        return "Binary heap is full"
+    else:
+        rootnode.customlist[rootnode.heapsize + 1] = nodevalue
+        rootnode.heapsize += 1
+        heapifytree(rootnode, rootnode.heapsize, heaptype)
+        return "Node inserted successfully"
+
+
+NewHeap = BinaryHeap(5)
+Insert(NewHeap, 4, "Min")
+Insert(NewHeap, 5, "Min")
+Insert(NewHeap, 2, "Min")
+Insert(NewHeap, 1, "Min")
+levelordertraversal(NewHeap)
