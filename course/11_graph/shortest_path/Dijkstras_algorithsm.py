59a048b4ebb03345f21b5b7f1cba95194b65b021
abhidharsh <abhidharsh1999@gmail.com>
2025-07-21 08:56:06 +0530

    learning Djikstra's algorithm

--- course/11_graph/shortest_path/Dijkstras_algorithsm.py
+++ course/11_graph/shortest_path/Dijkstras_algorithsm.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Edge:
     def __init__(self, weight, start_vertex, end_vertex):
         self.weight = weight
@@ -20,3 +23,29 @@ class Node:
     def addEdge(self, weight, desination_vertex):
         edge = Edge(weight, self, desination_vertex)
         self.neighbours.append(edge)
+
+
+# Djikstra Algoright
+class Djikstra:
+    def __init__(self):
+        self.heap = []
+
+    def calculate(self, start):
+        start.min_distance = 0
+        heapq.heappush(self.heap, start)
+
+        while self.heap:
+            # pop element with the lowest distance
+            actual_vertex = heapq.heappop(self.heap)
+
+            # consider the neightbours
+            for edge in actual_vertex.neighbours:
+                start = edge.start_vertex
+                target = edge.end_vertex
+                new_distance = start.min_distance + edge.weight
+                if new_distance < target.min_distance:
+                    target.min_distance = new_distance
+                    target.predecessor = start
+                    # update the heap
+                    heapq.heappush(self.heap, target)
+            actual_vertex.visited = True
