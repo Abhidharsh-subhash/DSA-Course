2864d103e27e00e70f724fdfc5d239c9f5f6a0e8
abhidharsh <abhidharsh1999@gmail.com>
2025-07-25 08:50:50 +0530

    Bellman Ford completed

--- course/11_graph/shortest_path/Bellman_ford.py
+++ course/11_graph/shortest_path/Bellman_ford.py
@@ -5,7 +5,7 @@ class Graph:
         self.nodes = []
 
     def add_edge(self, s, d, w):
-        self.graph.append[s, d, w]
+        self.graph.append([s, d, w])
 
     def add_node(self, value):
         self.nodes.append(value)
@@ -18,3 +18,32 @@ class Graph:
     def BellmanFord(self, src):
         dist = {i: float("inf") for i in self.nodes}
         dist[src] = 0
+
+        for _ in range(self.V - 1):
+            for s, d, w in self.graph:
+                if dist[s] != float("inf") and (dist[s] + w) < dist[d]:
+                    dist[d] = dist[s] + w
+
+        for s, d, w in self.graph:
+            if dist[s] != float("inf") and (dist[s] + w) < dist[d]:
+                print("Graph contains negative cycle")
+                return
+
+        return self.print_solution(dist)
+
+
+g = Graph(5)
+g.add_node("A")
+g.add_node("B")
+g.add_node("C")
+g.add_node("D")
+g.add_node("E")
+g.add_edge("A", "C", 6)
+g.add_edge("A", "D", 6)
+g.add_edge("B", "A", 3)
+g.add_edge("C", "D", 1)
+g.add_edge("D", "C", 2)
+g.add_edge("D", "B", 1)
+g.add_edge("E", "B", 4)
+g.add_edge("E", "D", 2)
+g.BellmanFord("E")
